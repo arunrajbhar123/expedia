531597c1adbe271292345a0c0ee870f79c0e29ea
Aakash928 <asassss337@gmail.com>
2022-06-17 11:46:32 +0530

    nav path added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import HotelPreview from "./components/HotelPageIPreview/HotelPreview.jsx";
+import WithSubnavigation from "./components/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
+     <WithSubnavigation/>
       <h1>Expedia</h1>
       <HotelPreview />
+      <Footer/>
     </div>
   );
 }
